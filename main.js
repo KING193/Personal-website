7bf262d972d521a1ec466c608e980c858cddc617
KING193 <123240288+KING193@users.noreply.github.com>
2023-12-19 10:04:20 -1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+	window.addEventListener('scroll', function (e) {
+		if (window.pageYOffset > 100) {
+			document.querySelector("header").classList.add('is-scrolling');
+		} else {
+			document.querySelector("header").classList.remove('is-scrolling');
+		}
+	});
+
+	const menu_btn = document.querySelector('.hamburger');
+	const mobile_menu = document.querySelector('.mobile-nav');
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileNav = document.querySelector('.mobile-nav');
+    const hamburger = document.querySelector('.hamburger');
+
+    mobileNav.addEventListener('click', function() {
+        this.classList.toggle('is-active');
+        hamburger.classList.toggle('is-active');
+    });
+});
