7baf32e6f1fdf670e889b4fdd4e62a861fefd3ef
KING193 <hichamohicham651@gmail.com>
2024-01-05 13:57:34 -1100

    edit

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.onload = function () {
 	window.addEventListener('scroll', function (e) {
 		if (window.pageYOffset > 100) {
@@ -17,7 +16,7 @@ window.onload = function () {
 	});
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded'), function() {
     const mobileNav = document.querySelector('.mobile-nav');
     const hamburger = document.querySelector('.hamburger');
 
@@ -25,32 +24,4 @@ document.addEventListener('DOMContentLoaded', function() {
         this.classList.toggle('is-active');
         hamburger.classList.toggle('is-active');
     });
-=======
-window.onload = function () {
-	window.addEventListener('scroll', function (e) {
-		if (window.pageYOffset > 100) {
-			document.querySelector("header").classList.add('is-scrolling');
-		} else {
-			document.querySelector("header").classList.remove('is-scrolling');
-		}
-	});
-
-	const menu_btn = document.querySelector('.hamburger');
-	const mobile_menu = document.querySelector('.mobile-nav');
-
-	menu_btn.addEventListener('click', function () {
-		menu_btn.classList.toggle('is-active');
-		mobile_menu.classList.toggle('is-active');
-	});
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    const mobileNav = document.querySelector('.mobile-nav');
-    const hamburger = document.querySelector('.hamburger');
-
-    mobileNav.addEventListener('click', function() {
-        this.classList.toggle('is-active');
-        hamburger.classList.toggle('is-active');
-    });
->>>>>>> 1dd658966f640c17f412e0a28b1d1f4daed6bc09
-});
